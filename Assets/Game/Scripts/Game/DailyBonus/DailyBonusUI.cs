d9f45cbe83ae6eec96962090943af15cb10ee9dc
Mephistos1 <b.m@dcjudgement.com>
2024-01-29 18:59:18 +0200

    develop UI and implement wheel spin, daily bonus

---
+++ Assets/Game/Scripts/Game/DailyBonus/DailyBonusUI.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+namespace Game.Scripts.Game.DailyBonus
+{
+    public class DailyBonusUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text coinTxt;
+
+        private int _day;
+        private RewardData _rewardData;
+
+        public RewardData RewardData => _rewardData;
+
+        public void SetRewardData(RewardData reward)
+        {
+            _rewardData = reward;
+        }
+
+        public void UpdateRewardsTxt()
+        {
+            coinTxt.gameObject.SetActive(true);
+            coinTxt.text = _rewardData.CoinReward + "";
+        }
+    }
+}
